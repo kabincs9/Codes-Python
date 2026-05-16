f09325355d71c91537d8337c53b9deef1d5e9174
kabincs9 <kabinbaniya9@gmail.com>
2026-05-16 22:57:32 +0545

    Add files via upload

---
+++ commandlinegreet.py
@@ -0,0 +1,9 @@
+from sys import argv
+
+if len(argv) == 2:
+   print(f"hello, {argv[1]}")
+else:
+    print("hello, world")
+# in this if we type two like .. .py and word like david ( $ phython commandlinegreet.py david)
+# then it will print hello, david
+## go to exit for next like exit type
