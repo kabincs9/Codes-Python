4ac51c965c107e88cdffdab2f0021d4486deddd7
kabincs9 <kabinbaniya9@gmail.com>
2026-05-16 22:58:03 +0545

    Add files via upload

---
+++ copy.py
@@ -0,0 +1,7 @@
+# no need like malloc strcpy with return ... toupper..
+
+s = input("s: ")
+t = s.capitalize()
+print(s) # or for only t use print(f"s: {s}")
+print(t)# print(f"t: {t}")
+# like very short than c
