0d52c1ccb3880470df902ffeb8768cb3f99004a3
kabincs9 <kabinbaniya9@gmail.com>
2026-05-16 22:57:18 +0545

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,12 @@
+from cs50 import get_int
+x = get_int("x: ")
+y = get_int("y: ")
+print(x + y)
+
+# or input for standard
+# not need from cs50...
+# x = input("x: ")
+# y = input("y: ")
+# print(x + y) will only print xy like 12 so there will be concatenation
+# so we need x = int(input("x: ")) .. y .. to work as above
+# so we need int function which does conversion for me
