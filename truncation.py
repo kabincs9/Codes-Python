0a8fadc635973bfde52d02e07f8cff2d35c68ac2
kabincs9 <kabinbaniya9@gmail.com>
2026-05-17 23:04:19 +0545

    Add files via upload

---
+++ truncation.py
@@ -0,0 +1,6 @@
+# to remove  like 3/2 ...1 truncation in python
+x = int(input("x: "))
+y = int(input("y: "))
+
+z = x / y  # no truncation by using / division in python
+print(z) # as # is used not //
