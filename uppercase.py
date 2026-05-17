a481718474c6cf56de9f28e230cdab451d3da33a
kabincs9 <kabinbaniya9@gmail.com>
2026-05-17 23:04:37 +0545

    Add files via upload

---
+++ uppercase.py
@@ -0,0 +1,7 @@
+before = input("Before: ")
+print("After: ", end="")# as i dont want new line to move so end
+for c in before: # for every character in c
+    print(c.upper(), end="")
+   print()
+# not thing = new line
+
