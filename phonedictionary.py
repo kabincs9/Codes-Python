ad0de1d56e70226b59082df1b9e639a50a06c8ee
kabincs9 <kabinbaniya9@gmail.com>
2026-05-17 23:02:54 +0545

    Add files via upload

---
+++ phonedictionary.py
@@ -0,0 +1,10 @@
+# go to csvphone first
+import csv
+
+name = input("Name: ")
+number = input("Number: ")
+
+with open("phonedictionary.csv", "a") as file:
+     writer = csv.DictWriter(file, fieldnames=["name", "number"])
+     writer.writerow({"name": name, "number": number})
+
